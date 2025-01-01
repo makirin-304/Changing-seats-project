9fcccdd0e354574ab4382136d8279db773c33542
rights-kitano <rights@rights-kitanonoMacBook-Pro.local>
2025-01-01 09:26:08 +0900

    arr.rb 追記

--- arr.rb
+++ arr.rb
@@ -1,3 +1,29 @@
 pp '人数を入力して下さい'
-member = gets.to_i
+input = gets.to_i
+
+if input < 10
+  puts 'エラー:10人以上で入力してください'
+end
+
+#配列の処理
+
+
+
+rem = input % 2
+mod6 = input % 6
+
+if rem == 0
+
+#偶数人の時の処理
+
+else if mod6 != 5
+
+#奇数人の時の処理
+
+else
+
+#奇数かつ６で割った余が５の時の処理
+
+end
+
 
